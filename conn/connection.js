6639220d5f4c0b6309edefddf830c85df162e441
jatin jain <jainjatin748@gmail.com>
2024-12-21 16:54:57 +0530

    first commit

---
+++ conn/connection.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import color from "colors";
+
+export const connectDB = async (req, res) => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI).then(() => {
+      console.log("Connected to the  Database".bgGreen.white);
+    });
+  } catch (error) {
+    console.log("Cannot connect to  Database".bgRed, error);
+    return res.status(400).json({
+      success: false,
+      message: "Cannot connect to Database",
+    });
+  }
+};
